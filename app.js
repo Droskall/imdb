a1929fbba0de8d5dfe26e145235df36e611190a2
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2022-01-04 15:16:57 +0100

    commit de secu

--- app.js
+++ app.js
@@ -1,35 +1,37 @@
-// Select the carousel you'll need to manipulate and the buttons you'll add events to
-const carousel = document.querySelector("[data-target='carousel']");
-const card = carousel.querySelector("[data-target='card']");
-const leftButton = document.querySelector("[data-action='slideLeft']");
-const rightButton = document.querySelector("[data-action='slideRight']");
+const swiper1 = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
 
-// In order to slide the carousel so that only three cards are perfectly visible each time,
-const carouselWidth = carousel.offsetWidth;
-const cardStyle = card.currentStyle || window.getComputedStyle(card)
-const cardMarginRight = Number(cardStyle.marginRight.match(/\d+/g)[0]);
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
 
-// Count the number of total cards you have
-const cardCount = carousel.querySelectorAll("[data-target='card']").length;
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
 
-// as well as a maxX property so the carousel knows when to stop at the upper limit
-let offset = 0;
-const maxX = -((cardCount / 3) * carouselWidth +
-    (cardMarginRight * (cardCount / 3)) -
-    carouselWidth - cardMarginRight);
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
 
-
-// Add the click events
-leftButton.addEventListener("click", function() {
-    if (offset !== 0) {
-        offset += carouselWidth + cardMarginRight;
-        carousel.style.transform = `translateX(${offset}px)`;
-    }
-})
-
-rightButton.addEventListener("click", function() {
-    if (offset !== maxX) {
-        offset -= carouselWidth + cardMarginRight;
-        carousel.style.transform = `translateX(${offset}px)`;
-    }
-})
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
