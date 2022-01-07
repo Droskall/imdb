d60c7e35d0124b0505197b31237ee45da90da8f8
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2022-01-07 10:58:58 +0100

    commit

---
+++ Carousel.js
@@ -0,0 +1,113 @@
+const Carousel = function (imagesArray, container, imgBalise, time, title, text, posters, captionContainer, upNextContainer) {
+    this.images = imagesArray;
+    this.container = container;
+    this.imgBalise = imgBalise;
+    this.time = time;
+    this.title = title;
+    this.text = text;
+    this.posters = posters
+    this.captionContainer = captionContainer;
+    this.upNextContainer = upNextContainer;
+
+    this.createHtmlBase = function () {
+        // img
+        let a = 0;
+
+        this.images.forEach(value => {
+            let img = document.createElement('img');
+            img.id = "image" + a.toString();
+            img.src = value;
+            img.alt = "image";
+            this.container.appendChild(img);
+
+            a++;
+        });
+
+        document.querySelector(this.captionContainer[3]).src = this.posters[0];
+    }
+
+    // for the swipe effect
+    let translate = 0;
+    let width;
+
+    // for events
+    this.previousTransition = function () {
+        width = document.querySelector(this.imgBalise).clientWidth;
+
+        // if first image go back to the last one
+        if (translate === 0)
+            translate = (imagesArray.length * width) - width;
+        else
+            translate -= width;
+
+        this.captionUpdate()
+        // translate images
+        document.querySelectorAll(imgBalise).forEach(value => {
+            value.style.transform = "translateX(" + (- translate) + "px)";
+        });
+    }
+
+    // for events
+    this.nextTransition = function () {
+        width = document.querySelector(imgBalise).clientWidth;
+
+        // if last image go back to the first one
+        if (translate === (imagesArray.length * width) - width)
+            translate = 0;
+        else
+            translate += width;
+
+        this.captionUpdate()
+
+        // translate images
+        document.querySelectorAll(imgBalise).forEach(value => {
+            value.style.transform = "translateX(-" + translate + "px)";
+        });
+    }
+
+    this.nextAuto = () => {
+        setInterval(() => this.nextTransition(), 8000)
+    }
+
+    // update the caption for each image
+    this.captionUpdate = () => {
+        let index = translate / width;
+
+        setTimeout(() => {
+            document.querySelector(this.captionContainer[0]).innerText = this.time[index];
+            document.querySelector(this.captionContainer[1]).innerText = this.title[index];
+            document.querySelector(this.captionContainer[2]).innerText = this.text[index];
+            document.querySelector(this.captionContainer[3]).src = this.posters[index];
+
+            this.upNextUpdate(index, this.upNextContainer[0], this.time);
+            this.upNextUpdate(index, this.upNextContainer[1], this.title);
+            this.upNextUpdate(index, this.upNextContainer[2], this.text);
+
+            let a = index;
+
+            document.querySelectorAll(this.upNextContainer[3]).forEach(value => {
+
+                if (a < this.images.length - 1)
+                    a++
+                else
+                    a = 0;
+
+                value.src = posters[a];
+            })
+        }, 500);
+    }
+
+    this.upNextUpdate = (index, container, array) => {
+        let a = index;
+
+        document.querySelectorAll(container).forEach(value =>{
+
+            if (a < this.images.length - 1)
+                a++
+            else
+                a = 0;
+
+            value.innerText = array[a]
+        })
+    }
+}
--- app.js
+++ app.js
@@ -13,3 +13,61 @@ let swiper = new Swiper(".mySwiper", {
         prevEl: ".swiper-button-prev",
     },
 });
+
+// carousel
+let images = [
+    "img/uncharted.png",
+    "img/movies.png",
+    "img/docteur-strange.png",
+    "img/series.png"
+];
+
+let time = [
+    "2:24",
+    "2:45",
+    "2:12",
+    "2:30"
+];
+
+let title = [
+    "Mark Walhberg & Tom Holland Go Treasure Hunting",
+    "The Most Anticipated New Movies of 2022",
+    "'Doctor Strange in the Multiverse of Madness'",
+    "IMDb Users' Most Anticipated Series",
+];
+
+let text = [
+    "Watch the new 'Uncharted' Trailer",
+    "Get Your Watchlists Ready",
+    "Watch the Teaser Trailer",
+    "New TV Coming in 2022"
+];
+
+let posters = [
+    "img/uncharted-poster.png",
+    "img/watchtowatch.png",
+    "img/docteur-strange-poster.png",
+    "img/watchtowatch.png"
+]
+
+const container = document.querySelector('.left div.nb1 div');
+const nextButton = document.querySelector('.left div.nb1 .next');
+const previousButton = document.querySelector('.left div.nb1 .previous');
+
+const captionContainer = [
+    ".bottom span",
+    ".bottom h2",
+    ".bottom p",
+    "#poster"
+]
+
+const upNextContainer = [
+    "div.text span",
+    "div.text h3",
+    "div.text p",
+    "div.images img"
+]
+
+let section1Carousel = new Carousel(images, container, 'div.nb1 img', time, title, text, posters, captionContainer, upNextContainer);
+section1Carousel.createHtmlBase();
+section1Carousel.nextAuto();
